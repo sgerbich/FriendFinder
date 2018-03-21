a5cc8c3d57200a71c215d63c6ad196a4b8133470
Sean <sdgerbich@gmail.com>
2018-03-21 13:55:49 -0400

    initial

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendsData = require("../data/friends");
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        // console.log(req.body);
+        friendsData.push(req.body);
+    });
+
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
